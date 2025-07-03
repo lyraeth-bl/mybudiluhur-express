c0498ba505d92ea83cae226179604b04a7e04f55
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2025-07-03 09:58:20 +0700

    feat: add index route for home page rendering

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 var { validateToken } = require("./lib/middleware/middleware");
 require("dotenv").config();
 
+var indexRouter = require("./routes/index");
 var notificationsRouter = require("./routes/notifications");
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
 app.use("/notifications", validateToken, notificationsRouter);
 
 // catch 404 and forward to error handler
